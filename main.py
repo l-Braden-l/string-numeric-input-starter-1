853cb21c63ed7a271e0019c8a37e06ae74226487
braden <leachbr787@northwested.org>
2024-09-13 14:04:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,36 @@
-# Student name
-# Current date
+# Braden Leach
+# Sep 9-13-24
 # String & Numeric Input 
 
 # Task 1
 # Comment
 # Comment
-gg = 6
+name = input('what is your name?: (example: fred)\n')
+carrer = input( 'what is the name of the program you are attending?: (examples: Graphic Arts)\n')
 
-
+print('Hello, '+ name + ' I really like the program '+ carrer +' too!')
 
 # Task 2
 # Comment
 # Comment
+int_age = int(input('what is your age: (example: 17)\n'))
 
-
-
+age_real = int(int_age) + 10
+print('Hello, did you know that you will be ' + str(age_real) + ' in 10 years!')
 
 # Task 3
 # Comment
 # Comment
-
-
+family = int(input('How many people are in your family?: (exaple: 2)\n'))
+print('The amount of people in your family are ' +str(family))
 
 
 # Task 4
 # Comment
 # Comment
-
+height = float(input('How tall are you in inches?: (example: 200)\n'))
+conversion_factor = height / 12
+print('you are '+ str(conversion_factor) + ' feet tall and ' + str(height) +  ' inches tall!')
 
 
 
